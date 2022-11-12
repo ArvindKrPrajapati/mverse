3593cfd5a9cebbec5be8703855a4deea4075cebb
ArvindKrPrajapati <arvindsoft37@gmail.com>
2022-11-12 22:01:15 +0530

    Added Splash Screen

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import { View, Text, StatusBar } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { theme } from './src/utils/theme'
 import RootNavigator from './src/navigation/RootNavigator'
+import SplashScreen from 'react-native-splash-screen'
+
+
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, [])
   return (
     <>
       <StatusBar backgroundColor={theme.colors.primary} barStyle="light-content" />
